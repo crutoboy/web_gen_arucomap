bd77dff3965a309efb34710fbb213d4003c670b1
crutoboy <vanyalex0192837465@gmail.com>
2026-06-29 20:54:24 +0000

    add test textarea; add comment to first line in map str

--- genmap.js
+++ genmap.js
@@ -2,7 +2,7 @@ function aruco_gen_str(
     length, markers_x, markers_y, dist_x, dist_y,
     first=0, x0=0, y0=0, bottom_left=false
 ){
-    let res = ''
+    let res = '# id\tlength\tx\ty\tz\trot_z\trot_y\trot_x\n'
     let max_y = y0 + (markers_y - 1) * dist_y
     for (let y = 0; y < markers_y; y++) {
         for (let x = 0; x < markers_y; x++){
